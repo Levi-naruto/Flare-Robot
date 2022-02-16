2d2d5c548dee489481b3131217f0de8c2699d822
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-02-16 19:03:58 +0530

    Update mmf.py

--- Flare_Robot/modules/mmf.py
+++ Flare_Robot/modules/mmf.py
@@ -13,149 +13,42 @@ from PIL import Image, ImageFont, ImageDraw
 
 from telegram.utils.helpers import mention_html
 from telegram import TelegramError, Update
-from telegram.ext import CallbackContext, run_async
+from telegram.ext import CallbackContext
 import cloudscraper
 import urllib.request as urllib
 
 
-Credit ="Kittu"
 
-@run_async
+
 @Flare(pattern="^/mmf ?(.*)")
-def handler(event):
+async def handler(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        event.reply("Reply to an image or a sticker to memeify it Nigga!!")
+        await event.reply("Reply to an image or a sticker to memeify it Nigga!!")
         return
-    reply_message = event.get_reply_message()
+    reply_message = await event.get_reply_message()
     if not reply_message.media:
-        event.reply("Provide some Text please")
+        await event.reply("Provide some Text please")
         return
-    file = bot.download_media(reply_message)
-    msg = event.reply("Memifying this image! Please wait")
+    file = await bot.download_media(reply_message)
+    msg = await event.reply("Memifying this image! Please wait")
 
     if "Kittu" in Credit:
        pass
 
     else: 
-       event.reply("this nigga removed credit line from code")
+       await event.reply("this nigga removed credit line from code")
     text = str(event.pattern_match.group(1)).strip()
 
     if len(text) < 1:
-        return msg.reply("You might want to try `/mmf text`")
-    meme = drawText(file, text)
-    bot.send_file(event.chat_id, file=meme, force_document=False)   
-    msg.delete()    
+        return await msg.reply("You might want to try `/mmf text`")
+    meme = await drawText(file, text)
+    await bot.send_file(event.chat_id, file=meme, force_document=False)   
+    await msg.delete()    
     os.remove(meme)
 
-@run_async
-def drawText(image_path, text):
-    img = Image.open(image_path)
-    os.remove(image_path)
-    shadowcolor = "black"
-    i_width, i_height = img.size
-    if os.name == "nt":
-        fnt = "ariel.ttf"
-    else:
-        fnt = "./Flate_Robot/resources/default.ttf"
-    m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
-    if ";" in text:
-        upper_text, lower_text = text.split(";")
-    else:
-        upper_text = text
-        lower_text = ""
-    draw = ImageDraw.Draw(img)
-    current_h, pad = 10, 5
-    if upper_text:
-        for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
-            draw.text(
-                xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
 
-            draw.text(
-                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-    if lower_text:
-        for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) - 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) + 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-    image_name = "memify.webp"
-    webp_file = os.path.join(image_name)
-    img.save(webp_file, "webp")
-    return webp_file
 
 # Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
 # Maybe replyed to suit the needs of this module
