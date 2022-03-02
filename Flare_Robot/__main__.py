1f972df10b9354427d3ef81c4d0a212ee9b0b057
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-02 09:35:44 +0530

    Update __main__.py

--- Flare_Robot/__main__.py
+++ Flare_Robot/__main__.py
@@ -661,7 +661,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[hey! I am back from OYO!!](https://telegra.ph/file/1ffcee4ce2da1c0d5dc49.mp4)", parse_mode=ParseMode.MARKDOWN)
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Hey Black Bulls! I am Awake Again as a Human [𓅩](https://telegra.ph/file/1ffcee4ce2da1c0d5dc49.mp4)!!", parse_mode=ParseMode.MARKDOWN)
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
