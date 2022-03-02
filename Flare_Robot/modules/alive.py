b42ccadbdb0759543ef8b4f6b6033391594123ca
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-02 09:24:41 +0530

    Update alive.py

--- Flare_Robot/modules/alive.py
+++ Flare_Robot/modules/alive.py
@@ -9,7 +9,7 @@ from Flare_Robot.events import register
 from Flare_Robot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/e450af5b3bf434d90cf65.jpg"
+PHOTO = "https://telegra.ph/file/ed6c1032e70b8a444c29f.jpg"
 
 
 @register(pattern=("/alive"))
@@ -18,12 +18,12 @@ async def awake(event):
     TEXT += f"**♡ I'm Working ᴘʀᴏᴘᴇʀʟʏ** \n\n"
     TEXT += f"**♡ sᴇᴄʀᴇ: LATEST Version** \n\n"
     TEXT += f"**♡ My Creator: [ naruto](http://t.me/lord_seventh_hokage_naruto)** \n\n"
-    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @nero_support** \n\n"
+    TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @nero_x_support** \n\n"
     TEXT += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ **"
     BUTTON = [
         [
-            Button.url(" Updates", "https://t.me/nero_updates"),
-            Button.url(" Support", "https://t.me/nero_support"),
+            Button.url(" Updates", "https://t.me/nero_x_updates"),
+            Button.url(" Support", "https://t.me/nero_x_support"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
